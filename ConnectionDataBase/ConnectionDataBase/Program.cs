50a2ff42aec6351f3dd94cae8ccfdcafab824507
Sonny Mayta <mayta.sonny@gmail.com>
2025-07-01 17:46:27 -0400

    ConnectionDataBase

---
+++ ConnectionDataBase/ConnectionDataBase/Program.cs
@@ -0,0 +1,14 @@
+﻿using Npgsql;
+
+var connString = "Host=localhost;Port=5432;Database=DBBackend;Username=postgres;Password=12345";
+
+try
+{
+    using var conn = new NpgsqlConnection(connString);
+    conn.Open();
+    Console.WriteLine("Conexión exitosa a PostgreSQL.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error al conectar: {ex.Message}");
+}
